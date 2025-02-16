df572b748b80ccabf15c5d23d072dff2e7c386b9
styv <styv2301@gmail.com>
2025-02-15 20:46:17 -0500

    feat: improve design and functionality of active links, including categories, difficulty, and search engine arrangement

--- src/common/layout/header/Header.tsx
+++ src/common/layout/header/Header.tsx
@@ -8,18 +8,20 @@ export const Header = () => {
   return (
     <header className="sticky top-0 z-40 bg-sonqu-white-400 drop-shadow-md">
       <div className="container flex h-14 items-center justify-between">
-        <Link href="/">
-          <Image
-            src="/logoSonqu.png"
-            alt="logo de sonqu"
-            className="h-auto w-24 object-contain"
-            width="100"
-            height="40"
-            priority
-          />
-        </Link>
-        <div className="flex items-center gap-2 lg:gap-6">
+        <div className="flex gap-7">
+          <Link href="/" className="my-auto">
+            <Image
+              src="/logoSonqu.png"
+              alt="logo de sonqu"
+              className="h-auto w-24 object-contain"
+              width="100"
+              height="40"
+              priority
+            />
+          </Link>
           <SearchNav />
+        </div>
+        <div className="flex items-center gap-2 lg:gap-6">
           <NavLinks />
           <MenuMobile />
         </div>
--- src/common/layout/header/Search.tsx
+++ src/common/layout/header/Search.tsx
@@ -1,18 +1,29 @@
-import { cn } from '@/lib/utils'
-import StylesButton from '../../styles/header.module.css'
 import { SearchIcon } from '@/common/components/icons/outline/SearchIcon'
+import { cn } from '@/lib/utils'
 import Link from 'next/link'
+import StylesButton from '../../styles/header.module.css'
 const SearchNav = () => {
   return (
+    // <div className="relative">
+    //   <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+    //   <input
+    //     type="search"
+    //     placeholder="¿Qué estás buscando?"
+    //     className="w-full bg-muted pl-8 pr-4"
+    //     // onFocus={() => setIsSearchFocused(true)}
+    //     // onBlur={() => setIsSearchFocused(false)}
+    //   />
+    // </div>
     <Link
       href={'/search'}
       className={cn(
         StylesButton.shadowSearch,
-        'mr-5 flex items-center justify-between rounded-md bg-white px-2.5 py-2.5 text-xs text-gray-500/80 hover:bg-white/80'
+        'mr-5 flex w-80 items-center justify-start rounded-md border border-[#44444415] bg-[#00000009] px-2.5 py-2.5 text-xs text-gray-500/80 hover:bg-white/5'
       )}
     >
       <SearchIcon className="h-4 w-4 sm:mr-2" aria-hidden="true" />
-      <span className="mr-2 hidden h-5 border-r border-gray-300 sm:inline-flex"></span>
+      <span className="hidden h-5 sm:inline-flex"></span>
+      {/* <span className="mr-2 hidden h-5 border-r border-gray-200 sm:inline-flex"></span> */}
       <span className="hidden px-1 sm:inline-flex">¿Qué esta buscando?</span>
     </Link>
   )
