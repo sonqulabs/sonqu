c319cb93f5315607e66c5dc155af750f05a1aa6a
alexander <alexander_pere@hotmail.com>
2025-05-19 16:56:38 -0500

    catalogo

--- src/common/layout/header/NavLinks.tsx
+++ src/common/layout/header/NavLinks.tsx
@@ -26,13 +26,21 @@ export const NavLinks = () => {
         })}
       </div>
       <Link
+        href="/catalogo"
+        className="group relative flex items-center gap-2 overflow-hidden rounded-lg bg-[#CE0146] px-3.5 py-1.5 font-medium text-white transition-all duration-300 hover:bg-[#CE0146] active:scale-95 active:bg-[#CE0146]"
+      >
+        <span className="absolute left-0 top-0 h-full w-full -translate-x-full transform bg-white opacity-0 transition-all duration-300 group-hover:translate-x-0 group-hover:opacity-20"></span>
+        <ShoppingBag className="h-4 w-4" />
+        <span className="font-monserrat text-sm font-normal">Catálogo</span>
+      </Link>
+      {/* <Link
         href="/tienda"
         className="group relative flex items-center gap-2 overflow-hidden rounded-lg bg-[#CE0146] px-3.5 py-1.5 font-medium text-white transition-all duration-300 hover:bg-[#CE0146] active:scale-95 active:bg-[#CE0146]"
       >
         <span className="absolute left-0 top-0 h-full w-full -translate-x-full transform bg-white opacity-0 transition-all duration-300 group-hover:translate-x-0 group-hover:opacity-20"></span>
         <ShoppingBag className="h-4 w-4" />
         <span className="font-monserrat text-sm font-normal">Tienda</span>
-      </Link>
+      </Link> */}
     </div>
   )
 }
